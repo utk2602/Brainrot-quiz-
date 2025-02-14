f5fed6ba418795d5729f3c076c654b16ac0bc83c
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-02-14 17:46:47 +0530

    ..

--- app/data/questions.ts
+++ app/data/questions.ts
@@ -4,10 +4,10 @@ export const questions = [
     question: "Fav meme? 🧠",
     type: "image",
     options: [
-      { image: "/doge.jpg", alt: "Doge 🐕✨ - Such wow" },
-      { image: "/pepe.jpg", alt: "Pepe 🐸💧 - Feels Guy" },
-      { image: "/Wojak_cropped.jpg", alt: "Wojak 🤔💭 - Sadboi Vibes" },
-      { image: "/gigachad.jpg", alt: "Gigachad 💪😎 - Sigma Male" },
+      { image: "/doge.jpg", alt: "Doge 🐕" },
+      { image: "/pepe.jpg", alt: "Pepe 🐸" },
+      { image: "/Wojak_cropped.jpg", alt: "Wojak 🤔" },
+      { image: "/gigachad.jpg", alt: "Gigachad 💪" },
     ],
   },
 
@@ -15,47 +15,28 @@ export const questions = [
     id: 2,
     question: "How long on screens? 📱",
     type: "text",
-    options: [
-      "2-4 hrs 📱 - Baby Steps 👶",
-      "4-6 hrs 📺 - Media King 👑",
-      "6-8 hrs 💻 - Procrastination 💼",
-      "8+ hrs 🧠 - Certified Brainrot 🧠",
-    ],
+    options: ["2-4 hrs 📱", "4-6 hrs 📺", "6-8 hrs 💻", "8+ hrs 🧠"],
   },
 
   {
     id: 3,
     question: "Brainrot vibe? 🧠",
     type: "text",
-    options: [
-      "Chaotic Neutral 🔥 - Meme Tornado 🌪️",
-      "Lawful Evil 😈 - Conspiracy Expert 🕵️‍♂️",
-      "Chaotic Good 🌈 - Meme Savior 🌍",
-      "True Neutral 🌊 - Dank Vibes 🦑",
-    ],
+    options: ["Chaotic 🔥", "Lawful 😈", "Good 🌈", "Neutral 🌊"],
   },
   
   {
     id: 4,
     question: "Meme vibe? 🌐",
     type: "text",
-    options: [
-      "Wholesome ❤️ - Pure Memes 🙏",
-      "Cursed 😈 - Reddit Lost 🛸",
-      "Dank 😎 - Unholy Trio ⚡",
-      "Surreal 🌌 - Glitchin' 🔮",
-    ],
+    options: ["Wholesome ❤️", "Cursed 😈", "Dank 😎", "Surreal 🌌"],
   },
   
   {
     id: 5,
-    question: "Go-to quote? 📢",
+    question: "Go-to yap genre ? 📢",
     type: "text",
-    options: [
-      "Touch grass 🌿 - Memes first 🌲",
-      "It's giving... 💅 - Meme Aesthetic 💅",
-      "No cap 🧢 - Facts, No Printer 🖨️",
-      "Yeet or be yeeted 💣 - Send it 🚀",
-    ],
+    options: ["Touch grass 🌿", "It's giving 💅", "No cap 🧢", "Yeet 💣"],
   },
-]
+  
+];
